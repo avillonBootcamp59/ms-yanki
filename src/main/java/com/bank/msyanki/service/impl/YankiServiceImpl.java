f2c85674b73a7253e0e5ea0c68b95965bd6e0409
aliciaVillon <villonalicia@gmail.com>
2025-03-06 23:52:56 -0500

    feat: se crea ms para monedero digital

--- src/main/java/com/bank/msyanki/service/impl/YankiServiceImpl.java
+++ src/main/java/com/bank/msyanki/service/impl/YankiServiceImpl.java
@@ -5,14 +5,16 @@ import com.bank.msyanki.repository.YankiRepository;
 import com.bank.msyanki.service.YankiService;
 import io.reactivex.rxjava3.core.Single;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class YankiServiceImpl implements YankiService {
 
     private final YankiRepository yankiRepository;
-
+    private final KafkaTemplate<String, String> kafkaTemplate;
     @Override
     public Single<YankiAccount> createAccount(YankiAccount account) {
         return Single.fromPublisher(yankiRepository.save(account));
@@ -22,4 +24,24 @@ public class YankiServiceImpl implements YankiService {
     public Single<YankiAccount> getAccountByPhone(String phone) {
         return Single.fromPublisher(yankiRepository.findByPhoneNumber(phone));
     }
+
+    @Override
+    public Mono<Void> sendPayment(String senderPhone, String receiverPhone, Double amount) {
+        return yankiRepository.findByPhoneNumber(senderPhone)
+                .flatMap(sender -> {
+                    if (sender.getBalance() < amount) {
+                        return Mono.error(new RuntimeException("Saldo insuficiente"));
+                    }
+                    sender.setBalance(sender.getBalance() - amount);
+                    return yankiRepository.save(sender)
+                            .then(yankiRepository.findByPhoneNumber(receiverPhone))
+                            .flatMap(receiver -> {
+                                receiver.setBalance(receiver.getBalance() + amount);
+                                kafkaTemplate.send("yanki-transactions", "Pago enviado de " + senderPhone + " a " + receiverPhone);
+                                return yankiRepository.save(receiver).then();
+                            });
+                });
+    }
+
+
 }
